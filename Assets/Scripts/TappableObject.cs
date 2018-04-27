a2685d40965bc26fbd657fd8c1565ee82c4c75cc
MeijerM1 <max1_meijer@hotmail.com>
2018-04-27 10:29:15 +0200

    Add basic movement

---
+++ Assets/Scripts/TappableObject.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TappableObject : MonoBehaviour
+{
+
+	// Trigger when player comes close to the object
+	private CapsuleCollider _enterTrigger;	
+	[SerializeField] [Range(1,7)] private float _enterRadius;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		_enterTrigger = gameObject.AddComponent<CapsuleCollider>();
+		_enterTrigger.radius = _enterRadius;
+		_enterTrigger.isTrigger = true;
+		_enterTrigger.center = new Vector3(0,0,0);
+	}
+	
+	// Draw the trigger radius in editor
+	void OnDrawGizmosSelected() {
+		Gizmos.color = Color.yellow;
+		Gizmos.DrawWireSphere(transform.position, _enterRadius);
+	}
+}
---
+++ Assets/Supercyan Character Pack Free Sample/Scripts/SimpleCharacterControl.cs
@@ -0,0 +1,278 @@
+﻿using System.Collections;
+using UnityEngine;
+using System.Collections.Generic;
+
+public class SimpleCharacterControl : MonoBehaviour {
+
+    private enum ControlMode
+    {
+        Tank,
+        Direct,
+        Tap
+    }
+
+    [SerializeField] private float m_moveSpeed = 2;
+    [SerializeField] private float m_turnSpeed = 200;
+    [SerializeField] private float m_jumpForce = 4;
+    [SerializeField] private Animator m_animator;
+    [SerializeField] private Rigidbody m_rigidBody;
+    [SerializeField] private Camera m_camera;    
+    
+    [SerializeField] private ControlMode m_controlMode = ControlMode.Direct;
+
+    private float m_currentV = 0;
+    private float m_currentH = 0;
+
+    private readonly float m_interpolation = 10;
+    private readonly float m_walkScale = 0.33f;
+    private readonly float m_backwardsWalkScale = 0.16f;
+    private readonly float m_backwardRunScale = 0.66f;
+
+    private bool m_wasGrounded;
+    private Vector3 m_currentDirection = Vector3.zero;
+
+    private float m_jumpTimeStamp = 0;
+    private float m_minJumpInterval = 0.25f;
+
+    private bool m_isGrounded;
+    private List<Collider> m_collisions = new List<Collider>();
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        ContactPoint[] contactPoints = collision.contacts;
+        for(int i = 0; i < contactPoints.Length; i++)
+        {
+            if (Vector3.Dot(contactPoints[i].normal, Vector3.up) > 0.5f)
+            {
+                if (!m_collisions.Contains(collision.collider)) {
+                    m_collisions.Add(collision.collider);
+                }
+                m_isGrounded = true;
+            }
+        }
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        ContactPoint[] contactPoints = collision.contacts;
+        bool validSurfaceNormal = false;
+        for (int i = 0; i < contactPoints.Length; i++)
+        {
+            if (Vector3.Dot(contactPoints[i].normal, Vector3.up) > 0.5f)
+            {
+                validSurfaceNormal = true; break;
+            }
+        }
+
+        if(validSurfaceNormal)
+        {
+            m_isGrounded = true;
+            if (!m_collisions.Contains(collision.collider))
+            {
+                m_collisions.Add(collision.collider);
+            }
+        } else
+        {
+            if (m_collisions.Contains(collision.collider))
+            {
+                m_collisions.Remove(collision.collider);
+            }
+            if (m_collisions.Count == 0) { m_isGrounded = false; }
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if(m_collisions.Contains(collision.collider))
+        {
+            m_collisions.Remove(collision.collider);
+        }
+        if (m_collisions.Count == 0) { m_isGrounded = false; }
+    }
+
+	void FixedUpdate () {
+        m_animator.SetBool("Grounded", m_isGrounded);
+
+        switch(m_controlMode)
+        {
+            case ControlMode.Direct:
+                DirectUpdate();
+                break;
+
+            case ControlMode.Tank:
+                TankUpdate();
+                break;
+            
+            case ControlMode.Tap:
+                TapUpdate();
+                break;
+            
+            default:
+                Debug.LogError("Unsupported state");
+                break;
+        }
+
+        m_wasGrounded = m_isGrounded;
+    }
+
+    private void TankUpdate()
+    {
+        float v = Input.GetAxis("Vertical");
+        float h = Input.GetAxis("Horizontal");
+
+        bool walk = Input.GetKey(KeyCode.LeftShift);
+
+        if (v < 0) {
+            if (walk) { v *= m_backwardsWalkScale; }
+            else { v *= m_backwardRunScale; }
+        } else if(walk)
+        {
+            v *= m_walkScale;
+        }
+
+        m_currentV = Mathf.Lerp(m_currentV, v, Time.deltaTime * m_interpolation);
+        m_currentH = Mathf.Lerp(m_currentH, h, Time.deltaTime * m_interpolation);
+
+        transform.position += transform.forward * m_currentV * m_moveSpeed * Time.deltaTime;
+        transform.Rotate(0, m_currentH * m_turnSpeed * Time.deltaTime, 0);
+
+        m_animator.SetFloat("MoveSpeed", m_currentV);
+
+        JumpingAndLanding();
+    }
+
+    private void DirectUpdate()
+    {
+        float v = Input.GetAxis("Vertical");
+        float h = Input.GetAxis("Horizontal");
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            v *= m_walkScale;
+            h *= m_walkScale;
+        }
+
+        m_currentV = Mathf.Lerp(m_currentV, v, Time.deltaTime * m_interpolation);
+        m_currentH = Mathf.Lerp(m_currentH, h, Time.deltaTime * m_interpolation);
+
+        Vector3 direction = m_camera.transform.forward * m_currentV + m_camera.transform.right * m_currentH;
+
+        float directionLength = direction.magnitude;
+        direction.y = 0;
+        direction = direction.normalized * directionLength;
+
+        if(direction != Vector3.zero)
+        {
+            m_currentDirection = Vector3.Slerp(m_currentDirection, direction, Time.deltaTime * m_interpolation);
+
+            transform.rotation = Quaternion.LookRotation(m_currentDirection);
+            transform.position += m_currentDirection * m_moveSpeed * Time.deltaTime;
+
+            m_animator.SetFloat("MoveSpeed", direction.magnitude);
+        }
+
+        JumpingAndLanding();
+    }
+
+    private void TapUpdate()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = m_camera.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                Transform objectHit = hit.transform;
+
+                if (hit.transform.GetComponent<TappableObject>() != null)
+                {
+                    Debug.Log("Start moving");
+                    StartCoroutine(Turn(hit.transform));
+                }
+            }
+        }
+    }
+
+    IEnumerator Turn(Transform target)
+    {
+        var localTarget = transform.InverseTransformPoint(target.position);
+     
+        var angle = Mathf.Atan2(localTarget.x, localTarget.z) * Mathf.Rad2Deg;
+        
+        while (angle < -5 || angle > 5)
+        {  
+            localTarget = transform.InverseTransformPoint(target.position);
+     
+            angle = Mathf.Atan2(localTarget.x, localTarget.z) * Mathf.Rad2Deg;
+            
+            var eulerAngleVelocity = new Vector3 (0, angle * 5, 0);
+            var deltaRotation = Quaternion.Euler(eulerAngleVelocity * Time.deltaTime );
+            m_rigidBody.MoveRotation(m_rigidBody.rotation * deltaRotation);
+            
+            yield return new WaitForSeconds(0.01f);
+            
+        }        
+        
+        MoveTowardsTarget(target);
+        yield return null;
+    }
+
+    private void MoveTowardsTarget(Transform target)
+    {
+        m_currentV = m_moveSpeed;
+        
+        Vector3 direction = m_camera.transform.forward * m_currentV + m_camera.transform.right * m_currentH;
+        
+        if(direction != Vector3.zero)
+        {
+            StartCoroutine(MoveObject(transform.position, target.position, 3.0f));
+            
+            m_animator.SetFloat("MoveSpeed", direction.magnitude);
+        }
+    }   
+
+    IEnumerator MoveObject(Vector3 startPos, Vector3 endPos, float time)
+    {
+        float i = 0.0f;
+        float rate = (float) 1.0/time;
+        while (i < 1.0) {
+            i += Time.deltaTime * rate;
+            transform.position = Vector3.Lerp(startPos, endPos, i);
+            yield return null; 
+        }
+    }
+
+    private void JumpingAndLanding()
+    {
+        bool jumpCooldownOver = (Time.time - m_jumpTimeStamp) >= m_minJumpInterval;
+
+        if (jumpCooldownOver && m_isGrounded && Input.GetKey(KeyCode.Space))
+        {
+            m_jumpTimeStamp = Time.time;
+            m_rigidBody.AddForce(Vector3.up * m_jumpForce, ForceMode.Impulse);
+        }
+
+        if (!m_wasGrounded && m_isGrounded)
+        {
+            m_animator.SetTrigger("Land");
+        }
+
+        if (!m_isGrounded && m_wasGrounded)
+        {
+            m_animator.SetTrigger("Jump");
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.gameObject.name);
+        
+        StopAllCoroutines();
+        
+        m_rigidBody.velocity = new Vector3(0,0,0);
+        m_rigidBody.angularVelocity = new Vector3(0,0,0);
+        
+        m_animator.SetFloat("MoveSpeed", 0);
+    }
+}

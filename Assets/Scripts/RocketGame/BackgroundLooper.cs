5e2162f1cec5b266f48a4d5d39587ba6a8596af8
Brent Schoenmakers <kofenoen@outlook.com>
2018-05-17 14:58:13 +0200

    background looper RocketGame
    not working atm

---
+++ Assets/Scripts/RocketGame/BackgroundLooper.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundLooper : MonoBehaviour
+{
+    public float Speed;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Vector2 offset = new Vector2(Time.time * Speed, 0);
+        GetComponent<Renderer>().material.mainTextureOffset = offset;
+	}
+}

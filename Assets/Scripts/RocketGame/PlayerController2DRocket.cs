fb20c360ade7ea3144300d318bf1db98b6f7199d
Brent Schoenmakers <kofenoen@outlook.com>
2018-05-17 14:25:35 +0200

    moving rocket and camera following

---
+++ Assets/Scripts/RocketGame/PlayerController2DRocket.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class PlayerController2DRocket : MonoBehaviour {
+
+    [SerializeField]
+    private float m_MaxSpeed = 10f;                    // The fastest the player can travel in the x axis.   
+   
+    private Rigidbody2D m_Rigidbody2D;
+    private bool m_FacingRight = true;  // For determining which way the player is currently facing.
+    private void Awake()
+    {
+        // Setting up references.
+        m_Rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        Move(1);
+    }
+    public void Move(float move)
+    {
+
+        //only control the player if grounded or airControl is turned on
+      
+            // Move the character
+            m_Rigidbody2D.velocity = new Vector2( m_Rigidbody2D.velocity.x, move * m_MaxSpeed);
+
+            // If the input is moving the player right and the player is facing left...
+            if (move > 0 && !m_FacingRight)
+            {
+                // ... flip the player.
+                Flip();
+            }
+            // Otherwise if the input is moving the player left and the player is facing right...
+            else if (move < 0 && m_FacingRight)
+            {
+                // ... flip the player.
+                Flip();
+            }
+        }
+
+
+    private void Flip()
+    {
+        // Switch the way the player is labelled as facing.
+        m_FacingRight = !m_FacingRight;
+
+        // Multiply the player's x local scale by -1.
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+
+}
+

17e8526724bfb599471f946c5a1a391697810521
Brent Schoenmakers <kofenoen@outlook.com>
2018-05-17 13:00:23 +0200

    added camera follow script to camera

---
+++ Assets/Scripts/RocketGame/CameraControl.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour {
+    public Transform Rocket;
+    public Transform emptyX;
+    public Transform emptyY;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Vector3 newposition = transform.position;
+        newposition.z = Rocket.position.z;
+        newposition.z = Mathf.Clamp(Rocket.z, emptyX.position.z, emptyY.position.z);
+        transform.position = newPosition;
+
+    }
+}

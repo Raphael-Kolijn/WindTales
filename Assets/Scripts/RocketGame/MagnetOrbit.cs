20e08b45ae99d7f40148991e19da18564382b1e7
Brent Schoenmakers <kofenoen@outlook.com>
2018-05-18 14:23:47 +0200

    magnet orbit with buttons

--- Assets/Scripts/RocketGame/MagnetOrbit.cs
+++ Assets/Scripts/RocketGame/MagnetOrbit.cs
@@ -1,17 +1,75 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+    
 
 public class MagnetOrbit : MonoBehaviour
 {
-    public float speed;
-    public Transform target;
-    
+    [Header("gameplay options")]
+    [SerializeField]
+    private DeviceManager.DeviceType deviceType;
 
-	// Use this for initialization
+    double flowRate;
 
-    void FixedUpdate()
+    [SerializeField]
+    [Tooltip("The amount which the player needs to inhale in order to attract coins")]
+    private double inhaleThreshold;
+
+    bool isPressedLeft = false;
+    bool isPressedRight = false;
+    float rotateSpeed = 100;
+    Transform test;
+    GameObject target;
+
+    void Awake()
     {
-        transform.RotateAround(target.position, Vector3.forward, speed);
+        DeviceManager.Instance.SetDeviceType(deviceType);
+    }
+    void Start()
+    {
+        test = transform.parent;
+        target = GameObject.Find("target");
+        
+    }
+
+    void Update()
+    {
+        flowRate = DeviceManager.Instance.FlowLMin;
+
+        flowRate = System.Math.Round(flowRate, 1);
+        if (isPressedLeft)
+        {
+            Rotate(-1);
+        }
+        if (isPressedRight)
+        {
+            Rotate(1);
+        }
+    }
+    public void onPointerDownLeft()
+    {
+        isPressedLeft = true;
+    }
+
+    public void onPointerUpLeft()
+    {
+        isPressedLeft = false;
+    }
+    public void onPointerDownRight()
+    {
+        isPressedRight = true;
+    }
+
+    public void onPointerUpRight()
+    {
+        isPressedRight = false;
+    }
+
+    public void Rotate(int whichWay)
+    {
+        transform.RotateAround(test.position, new Vector3(0, 0, whichWay), rotateSpeed * Time.deltaTime); // (1 is left) (-1 is right)
+       // transform.LookAt(target.transform.position); //Gives nullreferenceexception but still works?
+
     }
 }

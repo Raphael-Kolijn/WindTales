35e380410814f676f4e9ddbb5026d4d1f1291c87
Verhees, Nicky N <nicky.verhees@student.fontys.nl>
2018-05-24 13:02:05 +0200

    RocketGame Updated CoinScripts

---
+++ Assets/Scripts/RocketGame/CoinSpawner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinSpawner : MonoBehaviour
+{
+    public GameObject coin;
+
+    GameObject newCoin;
+    Vector3 newPosition;
+    Quaternion newRotation;
+    bool MaySpawn = false;
+
+	void Update ()
+    {
+        if (!MaySpawn)
+        {
+            newPosition = new Vector3(newCoin.transform.position.x, newCoin.transform.position.y + 10f, newCoin.transform.position.z);
+            newRotation = new Quaternion();
+            MaySpawn = !MaySpawn;
+
+            newCoin = Instantiate(coin, newPosition, newRotation);
+        }
+    }
+}

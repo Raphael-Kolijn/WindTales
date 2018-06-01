f42372a76d717315580c18d44f75a1e787074558
MeijerM1 <max1_meijer@hotmail.com>
2018-06-01 11:01:06 +0200

    Add chicken sound

--- Assets/Scripts/Hub/ChickenScript.cs
+++ Assets/Scripts/Hub/ChickenScript.cs
@@ -2,9 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ChickenScript : MonoBehaviour {
+public class ChickenScript : MonoBehaviour
+{
 
+	public AudioSource audio;
+	
 	    void Update() {
             transform.RotateAround(new Vector3(218, 50, 160), Vector3.up, 20 * Time.deltaTime);
         }
+
+	private void OnMouseDown()
+	{
+		audio.Play();
+	}
 }

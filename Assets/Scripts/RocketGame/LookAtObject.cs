005f3dca9b8fb67ac72d7ba5a9deb1e578132586
Brent Schoenmakers <kofenoen@outlook.com>
2018-06-07 14:25:51 +0200

    look script& models
    trying to make it so that the magnet rotates toward the coin. Also gave the  rocket and magnet a new look

---
+++ Assets/Scripts/RocketGame/LookAtObject.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtObject : MonoBehaviour
+{
+    public Transform Target;
+    public float RotateSpeed;   
+
+    
+	// Update is called once per frame
+	void Update ()
+    {
+        var direction = Target.transform.position - transform.position;
+
+        // Set Y the same to make the rotations turret-like:
+        direction.y = transform.position.y;
+
+        var rot = Quaternion.LookRotation(direction, Vector3.up);
+        transform.rotation = Quaternion.RotateTowards(
+                                         transform.rotation,
+                                         rot,
+                                         RotateSpeed * Time.deltaTime);   
+
+    }
+}

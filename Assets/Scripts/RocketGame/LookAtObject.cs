434e01af8dfa760fb03153c1b1b0e630be689711
Verhees, Nicky N <nicky.verhees@student.fontys.nl>
2018-06-08 13:47:50 +0200

    RocketGame UI updated

--- Assets/Scripts/RocketGame/LookAtObject.cs
+++ Assets/Scripts/RocketGame/LookAtObject.cs
@@ -4,23 +4,31 @@ using UnityEngine;
 
 public class LookAtObject : MonoBehaviour
 {
-    public Transform Target;
-    public float RotateSpeed;   
+    public GameObject Target;
+    public float RotateSpeed;
 
-    
-	// Update is called once per frame
-	void Update ()
+    // Update is called once per frame
+    void Update ()
     {
+        Target = GetComponent<FieldOfView>().foundCoin;
+        if (Target != null)
+        {
+            transform.LookAt(Target.transform, Vector3.forward);
+        }
+        /*
         var direction = Target.transform.position - transform.position;
 
         // Set Y the same to make the rotations turret-like:
-        direction.y = transform.position.y;
+        direction.z = transform.position.z;
 
-        var rot = Quaternion.LookRotation(direction, Vector3.forward);
+        
+        var rot = Quaternion.LookRotation(direction, Vector3.up);
+        
         transform.rotation = Quaternion.RotateTowards(
                                          transform.rotation,
                                          rot,
-                                         RotateSpeed * Time.deltaTime);   
-
+                                         RotateSpeed * Time.deltaTime);
+        */
+        //transform.Rotate(0, 100 * Time.deltaTime, 0);
     }
 }
---
+++ Assets/Scripts/RocketGame/ReturnToHub.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.SceneManagement;
+
+public class ReturnToHub : MonoBehaviour
+{
+    public void Return()
+    {
+        EditorSceneManager.LoadScene(0);
+    }
+}

f542d2c875e4c9a595c6980c93e2fc051e3e9375
MeijerM1 <max1_meijer@hotmail.com>
2018-06-14 09:10:11 +0200

    Add shop sounds

--- Assets/Scripts/Hub/Shop.cs
+++ Assets/Scripts/Hub/Shop.cs
@@ -7,6 +7,7 @@ public class Shop : TappableObject
 	public ShopItem[] Items;
 	public ShopItemUi ShopItemUi;
 	public CoinScript CoinManager;
+	public HubAudioManager AudioManager;
 	
 	void Start () {
 		InitialiseTrigger();
@@ -14,12 +15,16 @@ public class Shop : TappableObject
 
 	public override void OpenUi()
 	{
+		AudioManager.PlaySound("MenuOpen");
+		
 		_uiInstance.GetComponent<ShopUi>().ClearShop();
+		_uiInstance.GetComponent<ShopUi>().AudioManager = AudioManager;
 		_uiInstance.SetActive(true);
 		foreach (var shopItem in Items)
 		{
 			ShopItemUi itemUi = Instantiate(ShopItemUi);
 			itemUi.SetData(shopItem);
+			itemUi.AudioManager = AudioManager;
 			itemUi.CoinManager = CoinManager;
 			itemUi.shopUi = _uiInstance.GetComponent<ShopUi>();
 			
